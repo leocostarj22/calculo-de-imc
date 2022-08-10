cb93ee28835d642bd167d275fb112096cc60cc05
Leonardo Costa <103146352+leocostarj22@users.noreply.github.com>
2022-08-10 14:24:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -33,17 +33,17 @@ function handleButtonClick() {
   if(imc <16){
     imcResult.textContent = (`Valor inválido!`);
   } else if(imc >= 16 && imc <17){
-    imcResult.textContent = (`O IMC desses dados é ${imc}, Este valor considera que você está na faixa "abaixo do peso". `);
+    imcResult.textContent = (`O IMC desses dados é ${formattedImc}. Este valor considera que você está na faixa "abaixo do peso". `);
   } else if(imc >= 17 && imc <18,5){
-    imcResult.textContent = (`O IMC desses dados é ${imc}, Este valor considera que você está na faixa "Peso Normal". `); 
+    imcResult.textContent = (`O IMC desses dados é ${formattedImc}. Este valor considera que você está na faixa "Peso Normal". `); 
   } else if(imc >= 18.5 && imc <25){
-    imcResult.textContent = (`O IMC desses dados é ${imc}, Este valor considera que você está na faixa "Acima do Peso". `);  
+    imcResult.textContent = (`O IMC desses dados é ${formattedImc}. Este valor considera que você está na faixa "Acima do Peso". `);  
   } else if(imc >= 25 && imc <30){
-    imcResult.textContent = (`O IMC desses dados é ${imc}, Este valor considera que você está na faixa "Obesidade Grau I". `);  
+    imcResult.textContent = (`O IMC desses dados é ${formattedImc}. Este valor considera que você está na faixa "Obesidade Grau I". `);  
   } else if(imc >= 30 && imc <35){
-    imcResult.textContent = (`O IMC desses dados é ${imc}, Este valor considera que você está na faixa "Obesidade Grau II". `);  
+    imcResult.textContent = (`O IMC desses dados é ${formattedImc}. Este valor considera que você está na faixa "Obesidade Grau II". `);  
   } else if(imc > 40){
-    imcResult.textContent = (`O IMC desses dados é ${imc}, Este valor considera que você está na faixa "Obesidade Grau III". `);  
+    imcResult.textContent = (`O IMC desses dados é ${formattedImc}. Este valor considera que você está na faixa "Obesidade Grau III". `);  
   }  
   return;
 }
